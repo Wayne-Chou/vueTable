ad8ce779cc174e3d73f56c50d6816041053ca112
hank Name <hank8501167@gmail.com>
2024-01-11 15:28:20 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-createApp(App).use(store).mount('#app')
+const axiosInstance = axios.create({
+    headers: { "Content-Type": "application/json" },
+});
+
+const app = createApp(App);
+app.use(store).use(router);
+app.config.globalProperties.$axios = { ...axiosInstance };
+app.mount("#app");
